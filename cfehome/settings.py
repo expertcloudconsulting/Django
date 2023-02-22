e61b3aed862b7058174d34bc2d01e163dfb920d7
GPUONCLOUD <38043584+expertcloudconsulting@users.noreply.github.com>
2023-02-22 17:36:17 +0530

    Update settings.py

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -1,3 +1,33 @@
+
+import os
+
+DB_USERNAME=os.environ.get("DB_USERNAME")
+DB_PASSWORD=os.environ.get("DB_PASSWORD")
+DB_HOST=os.environ.get("DB_HOST")
+DB_PORT=os.environ.get("DB_PORT")
+DB_DATABASE=os.environ.get("DB_DATABASE")
+DB_IS_AVAIL = all([
+        DB_USERNAME, 
+        DB_PASSWORD, 
+        DB_HOST,
+        DB_PORT,
+        DB_DATABASE
+])
+
+if DB_IS_AVAIL:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": DB_DATABASE,
+            "USER": DB_USERNAME,
+            "PASSWORD": DB_PASSWORD,
+            "HOST": DB_HOST,
+            "PORT": DB_PORT,
+        }
+    }
+
+
+
 """
 Django settings for cfehome project.
 
@@ -9,6 +39,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
 
 from pathlib import Path
 
